4d88c6b391cd6da30283a7aa11eb22529534cc44
Alexandre Roux <alex@tekartik.com>
2026-02-01 21:30:45 +0100

    fix lints

--- packages/stripe/lib/src/payment_link_model.dart
+++ packages/stripe/lib/src/payment_link_model.dart
@@ -80,10 +80,7 @@ extension StripeApiPaymentLinkExt on StripeApiPaymentLink {
   Uri uriWith({String? email, String? locale}) {
     var uri = Uri.parse(url.v!);
     uri = uri.replace(
-      queryParameters: {
-        if (email != null) 'prefilled_email': email,
-        if (locale != null) 'locale': locale,
-      },
+      queryParameters: {'prefilled_email': ?email, 'locale': ?locale},
     );
     return uri;
   }
